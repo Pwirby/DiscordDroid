6cdcc616b0746e758fc835449ba04d35548acd16
Pwirby <jannel.clyde@gmail.com>
2022-01-27 21:02:32 +0100

    🐛 Fix exports

---
+++ src/commands/replies.ts
@@ -0,0 +1,15 @@
+export const working = [
+    "Give me a second, I'm on it 🥵",
+    "Awesome, I'm almost there 👨‍🎨",
+];
+
+export const replieFail = [
+    "Sorry, there was an issue loading the picture, are you sure that <URL> is valid ? 😰",
+    "Something went wrong, I could not get that picture, are you sure that <URL> is valid ? 😖",
+    "Oopsie, I did not manage to grab that picture, are you sure that <URL> is valid ? 🤥",
+];
+
+export const replieSuccess = [
+    "Hot from the CPU ! 🥧",
+    "The icing on the JPEG ! 🍰",
+];
