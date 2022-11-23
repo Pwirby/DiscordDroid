b3f1bbc1c82565a1fa9633a807f2017a568dba3f
Clyde JANNEL <jannel.clyde@gmail.com>
2022-11-23 10:56:20 +0100

    Fix loose condition

--- src/commands/rps.ts
+++ src/commands/rps.ts
@@ -4,34 +4,44 @@ import { Command } from "../command";
 const allResults = ["rock", "paper", "scissors"];
 
 export const rps: Command = {
-    data: new SlashCommandBuilder()
-        .setName("rps")
-        .setDescription("Plays a game of rock paper scissor against the bot")
-        .addStringOption(option => option
-            .setName("choice")
-            .setDescription("Rock paper or scissor")
-            .setRequired(true)),
-    run: async (interaction) => {
-        let choice = interaction.options.getString("choice");
-        if (choice && allResults.includes(choice)) {
-            // Bot choose at random
-            let index = Math.floor(Math.random() * allResults.length);
-            // We get the string corresponding to the bot's choice
-            let result = allResults[index];
-            // We compare user's choice with the random choice
-            if (choice === result) {
-                interaction.reply(`You played ${choice}, and I played ${result} too ! That mean it's a draw 🤝`);
-            } else {
-                [["rock", "paper"],
-                ["scissor", "rock"],
-                ["paper", "scissor"]].forEach(element => {
-                    if (true) {
-                        interaction.reply(`You played ${choice}, but guess what : I played ${result} ! I won ! 💪`);
-                    }
-                });
-                interaction.reply(`You played ${choice}, and I played ${result} ! You won ! 🏆`);
-            }
-        }
+  data: new SlashCommandBuilder()
+    .setName("rps")
+    .setDescription("Plays a game of rock paper scissor against the bot")
+    .addStringOption((option) =>
+      option
+        .setName("choice")
+        .setDescription("Rock paper or scissor")
+        .setRequired(true)
+    ),
+  run: async (interaction) => {
+    let choice = interaction.options.getString("choice", true);
+    if (choice && allResults.includes(choice)) {
+      // Bot choose at random
+      let index = Math.floor(Math.random() * allResults.length);
+      // We get the string corresponding to the bot's choice
+      let result = allResults[index];
+      // We compare user's choice with the random choice
+      if (choice === result) {
+        interaction.reply(
+          `You played ${choice}, and I played ${result} too ! That mean it's a draw 🤝`
+        );
+      } else {
+        [
+          ["rock", "paper"],
+          ["scissor", "rock"],
+          ["paper", "scissor"],
+        ].forEach((element) => {
+          if (element[0] === choice && element[1] === result) {
+            interaction.reply(
+              `You played ${choice}, but guess what : I played ${result} ! I won ! 💪`
+            );
+          } else {
+            interaction.reply(
+              `You played ${choice}, and I played ${result} ! You won ! 🏆`
+            );
+          }
+        });
+      }
     }
-}
-
+  },
+};
